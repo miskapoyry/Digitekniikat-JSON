24351748fe4359314d33df63aa9feaa48b90e8b4
Miska Poyry <bhd692@myy.haaga-helia.fi>
2025-02-19 01:40:57 +0200

    Tee tehtava 3

---
+++ teht3js/teht3.js
@@ -0,0 +1,38 @@
+function fetchEvents() {
+  // Osoite josta fetchataan
+  fetch("https://api.visittampere.com/api/v1/visittampere/event/published/all/?for mat=json&lang=fi")
+    // Muunnetaan vastaus JSON muotoon nuolinotaatiofunktiolla
+
+    .then((response) => response.json())
+
+    // Käsitellään muunnettu (eli JSON muotoinen) vastaus
+    // Kutsutaan funktiota ja välitetään sille json-vastaus tapahtumat(responseJson)})
+
+    .then((data) => tapahtumat(data))
+
+    // Jos tuli jokin virhe
+    .catch((error) => {
+      // TÄMÄ VIESTI NÄYTETÄÄN ERRORIN SATTUESSA
+      document.getElementById("vastaus").innerHTML =
+        "<p>Tietoa ei pystytä hakemaan </p>" + error;
+    });
+}
+
+function tapahtumat(data) {
+
+    var teksti = "";
+    teksti = "<h1>Tampereella tapahtuu</h1><br />";
+    
+    // LOOPATAAN JSON RESPONSEN LÄPI JA LAITETAAN JOKAISEN SILMUKAN SISÄLTÖ TEKSTIIN
+    for (var i = 0; i < data.length; i++) {
+    // DATA.TITLE HAKEE OTSIKON
+    teksti = teksti + "<h3>" + data[i].title + "</h3>";
+    // DATA.DESCRIPTION HAKEE KUVAUKSEN
+    teksti = teksti + "<p>" + data[i].description + "</p>";
+    // DATA.URL HAKEE TAPAHTUMAN URL
+    teksti = teksti + "<p> <a href=" + data[i].url + ">" + data[i].url + "</a></p><hr />";
+    }
+    
+    // TEKSTI LAITETAAN HTML SIVUN DIVIIN, JOKA ID ON "VASTAUS"
+    document.getElementById("vastaus").innerHTML = teksti;
+    }
