51b92d724e82d412f2e28dcf0825e74feef328b1
Miska Poyry <bhd692@myy.haaga-helia.fi>
2025-02-19 02:02:16 +0200

    Aloita saa tehtava

--- teht3js/teht3.js
+++ teht3js/teht3.js
@@ -36,3 +36,26 @@ function tapahtumat(data) {
     // TEKSTI LAITETAAN HTML SIVUN DIVIIN, JOKA ID ON "VASTAUS"
     document.getElementById("vastaus").innerHTML = teksti;
     }
+
+function fetchWeather() {
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=Helsinki,FI&appid=665ecd56dfc08dbb50feb8b8f5034e28&units=metric")
+    // Muunnetaan vastaus JSON muotoon nuolinotaatiofunktiolla
+
+    .then((response) => response.json())
+
+    // Käsitellään muunnettu (eli JSON muotoinen) vastaus
+    // Kutsutaan funktiota ja välitetään sille json-vastaus tapahtumat(responseJson)})
+
+    .then((data) => saa(data))
+
+    // Jos tuli jokin virhe
+    .catch((error) => {
+      // TÄMÄ VIESTI NÄYTETÄÄN ERRORIN SATTUESSA
+      document.getElementById("vastaus").innerHTML =
+        "<p>Tietoa ei pystytä hakemaan </p>" + error;
+    });
+}
+
+function saa(data){
+    
+}
